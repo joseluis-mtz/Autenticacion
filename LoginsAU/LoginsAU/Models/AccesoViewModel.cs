f38afefff5ae59b344d0216a831b151af8e5351e
Jose Luis Martinez <joseluismartinez1024@gmail.com>
2023-07-21 18:49:02 -0600

    Modelo acceso
    Crear modelo de acceso

---
+++ LoginsAU/LoginsAU/Models/AccesoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginsAU.Models
+{
+    public class AccesoViewModel
+    {
+        [Required(ErrorMessage = "Email obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password obligatorio")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Contraseña")]
+        public string Password { get; set; }
+
+        // Para recordar las credenciales del usuario
+        [Display(Name = "Recordarme")]
+        public bool RememberMe { get; set; }
+    }
+}
